b6d5a5d23c64c0dac1cca60740479181f78c42bf
SedaII <i.messnaoui@gmail.com>
2021-02-17 21:51:05 +0100

    Started Product Details Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => {
+    return p._id === match.params.id
+  })
   return (
-    <>
-    </>
+    <div>
+    {product.name}
+    </div>
   )
 }
 
